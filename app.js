b69fe188ed9a8478cd6f79b9e106b184870d9c4b
Mona Lisa <david.t.bui83@gmail.com>
2020-06-15 23:07:57 -0700

    saved before pushing

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const morgan = require('morgan');
 
 /////////////////////////////////////////
 // MIDDLEWARE 
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`/${__dirname}/public`));
 
