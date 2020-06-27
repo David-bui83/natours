ca804d7983e5ee1573979584cc39157f3abcd680
Mona Lisa <david.t.bui83@gmail.com>
2020-06-26 18:35:23 -0700

    done with delete current user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,8 @@ const {
   getUser,
   updateUser,
   deleteUser,
-  updateMe
+  updateMe,
+  deleteMe
 } = require('../controllers/usersController');
 const { 
   signup, 
@@ -27,6 +28,7 @@ router.patch('/resetPassword/:token', resetPassword);
 router.patch('/updateMyPassword', protect, updatePassword);
 
 router.patch('/updateMe', protect, updateMe);
+router.delete('/deleteMe', protect, deleteMe);
 
 router
   .route('/')
