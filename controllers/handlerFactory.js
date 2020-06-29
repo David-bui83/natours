f4cd46e02837e48c2656c9d74f04a1a8a7469a32
Mona Lisa <david.t.bui83@gmail.com>
2020-06-28 21:27:04 -0700

    added indexing for tourModel

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -78,6 +78,7 @@ exports.getAll = Model => catchAsync(async (req, res, next) => {
                     .limitFields()
                     .paginate();
 
+  // const doc = await features.query.explain();
   const doc = await features.query;
 
   // SEND RESPONSE
