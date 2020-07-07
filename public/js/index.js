7c071471d55d1f7abc3a340c5d849fddf988dfb9
Mona Lisa <david.t.bui83@gmail.com>
2020-07-07 14:19:37 -0700

    done with stripe front-end

--- public/js/index.js
+++ public/js/index.js
@@ -3,13 +3,15 @@ import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
+import { bookTour } from './stripe';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map')
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
-const userPasswordForm = document.querySelector('.form-user-password')
+const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
 
 // DELEGATION
 if(mapBox) {
@@ -59,5 +61,13 @@ if(userPasswordForm) {
     document.getElementById('password-current').value = '';
     document.getElementById('password').value = '';
     document.getElementById('password-confirm').value = '';
+  });
+}
+
+if(bookBtn) {
+  bookBtn.addEventListener('click', e => {
+    e.target.textContent = 'Processing...'
+    const { tourId } = e.target.dataset;
+    bookTour(tourId);
   });
 }
---
+++ public/js/stripe.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe('pk_test_51H22XcFxGUAyaf7wqZzoqJSikvYMgRQ2ULCpoQJw3la4JFnzPP1ZipdtTEmOjmNhISQ5doOJ1Yeo37wA0TUx459o00SkYqEUI4');
+
+export const bookTour = async tourId => {
+  try{
+    console.log(tourId);
+    // 1) Get checkout session from API
+    const session = await axios(`http://localhost:5000/api/v1/bookings/checkout-session/${tourId}`);
+    console.log(session);
+  
+    // 2) Create checkout form + charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  }catch(err){
+    console.log(err);
+    showAlert('error', err);
+  }
+};
