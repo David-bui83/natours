ee88294c5d88bc0764ec16eb124bee143cb0827b
Mona Lisa <david.t.bui83@gmail.com>
2020-07-07 19:00:26 -0700

    done with booking api

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,14 +1,34 @@
 const express = require('express');
-const { 
+const {
+  createBooking,
+  getBooking,
+  getAllBookings,
+  updateBooking,
+  deleteBooking, 
   getCheckoutSession
 } = require('../controllers/bookingController');
 const {
-  protect
+  protect,
+  restrictTo
 } = require('../controllers/authController');
 
 const router = express.Router();
 
+router.use(protect);
+
 router.get('/checkout-session/:tourId', protect, getCheckoutSession);
 
+router.use(restrictTo('admin', 'lead-guide'));
+
+router
+  .route('/')
+  .get(getAllBookings)
+  .post(createBooking);
+
+router
+  .route('/:id')
+  .get(getBooking)
+  .patch(updateBooking)
+  .delete(deleteBooking);
 
 module.exports = router;
