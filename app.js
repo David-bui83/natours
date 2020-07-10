67c3c48ddb2c06d76a4f3b2c7da08e70941e826c
Mona Lisa <david.t.bui83@gmail.com>
2020-07-09 22:29:20 -0700

    Improved stripe implementation

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
-const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -59,6 +60,13 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour!'
 });
 app.use('/api', limiter);
+
+// stripe webhook
+app.post(
+  '/webhook-checkout', 
+  express.raw({type: 'application/json'}),
+  bookingController.webhookCheckout
+);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -12,17 +12,26 @@ const {
   protect 
 } = require('../controllers/authController');
 const {
-  createBookingCheckout
+  // createBookingCheckout
 } = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get('/', createBookingCheckout, isLoggedIn, getOverview);
+router.get('/', isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLoginForm);
 router.get('/me', protect, getAccount);
-router.get('/my-tours', protect, getMyTours);
+router.get(
+  '/my-tours', 
+  // createBookingCheckout, 
+  protect, 
+  getMyTours
+);
 
-router.post('/submit-user-data', protect, updateUserData);
+router.post(
+  '/submit-user-data',
+  protect, 
+  updateUserData
+);
 
 module.exports = router;
