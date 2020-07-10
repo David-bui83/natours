9f64432eaaf0a673cc46346bfd877ee626640832
Mona Lisa <david.t.bui83@gmail.com>
2020-07-09 20:59:23 -0700

    made more changes

--- app.js
+++ app.js
@@ -26,9 +26,6 @@ app.enable('trust proxy');
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-// serviing static files
-// app.use(express.static(`/${__dirname}/public`));
-app.use(express.static(path.join(__dirname, 'public')));
 
 /////////////////////////////////////////
 // GLOBAL MIDDLEWARE 
@@ -42,6 +39,10 @@ app.use(cors());
 // }))
 
 app.options('*'. cors());
+
+// serviing static files
+// app.use(express.static(`/${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
 app.use(helmet());
